d0a38cadba3ebb9ae82b4167f99c14d2f674fe3c
Lucas Capuano <lucascapleal@gmail.com>
2026-01-23 19:44:29 -0300

    First commit

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,15 @@ from flask_cors import CORS
 from flask_smorest import Api
 from .api.transactions import blp as transactions_blp
 from .api.balance import blp as balance_blp
+from .api.auth import blp as auth_blp
+import os
+
+
+from flask_jwt_extended import JWTManager
+
+# Setup the Flask-JWT-Extended extension
+
+
 
 def create_app():
     app = Flask(__name__)
@@ -10,10 +19,15 @@ def create_app():
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/docs"
+
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "@super-secretKEY123")
+    jwt = JWTManager(app)
+
     CORS(app)
     api = Api(app)
     api.register_blueprint(transactions_blp)
     api.register_blueprint(balance_blp)
+    api.register_blueprint(auth_blp)
 
     @app.get("/health")
     def health():
